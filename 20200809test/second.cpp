706c1b4b31f7bb4263cad7274e4784cec3fae28d
kzt206 <kzt206@gmail.com>
2020-08-09 18:28:45 +0900

    modify second.cpp

--- 20200809test/second.cpp
+++ 20200809test/second.cpp
@@ -4,13 +4,15 @@ int main(void){
     int wa,sa,seki,syo,amari;
     int num1 = 2,num2 = 5;
 
-    wa = 1 + 1;
-    sa = 10 -3;
-    seki = 3* 8;
-    syo = 10/5;
-    amari = 10&6;
+    scanf("%d%d",&num1,&num2);
 
-    printf("wa = %d, sa = %d, seki = %d, syo = %d\n",wa,sa,seki,syo);
+    wa = num1 + num2;
+    sa = num1 - num2;
+    seki = num1 * num2;
+    syo = num1/num2;
+    amari = num1%num2;
+
+    printf("wa = %d, sa = %d, seki = %d, syo = %d, amari = %d\n",wa,sa,seki,syo,amari);
     
     return 0;
 }
