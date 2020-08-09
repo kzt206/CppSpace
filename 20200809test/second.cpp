81cd6087d87851c53a98a2030d0c776479c77ace
kzt206 <kzt206@gmail.com>
2020-08-09 18:23:57 +0900

    add second.cpp

---
+++ 20200809test/second.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+    int wa,sa,seki,syo,amari;
+    int num1 = 2,num2 = 5;
+
+    wa = 1 + 1;
+    sa = 10 -3;
+    seki = 3* 8;
+    syo = 10/5;
+    amari = 10&6;
+
+    printf("wa = %d, sa = %d, seki = %d, syo = %d\n",wa,sa,seki,syo);
+    
+    return 0;
+}
